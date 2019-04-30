5c5dac98679dda494c4688de43107a7755d32218
Ben Turley <n10203877@qut.edu.au>
2019-04-30 22:22:02 +1000

    Initial Line implementation

---
+++ src/TestingClass.java
@@ -0,0 +1,6 @@
+public class TestingClass {
+    public static void main(String[] args) {
+        Line test = new Line(0, 0, 1, 1);
+        System.out.println(test.lineOutputFormatted());
+    }
+}
