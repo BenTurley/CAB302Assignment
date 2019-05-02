3d82030b9a4258c0a8a9bb7ca32d88eed107b978
chris <chris.thomas@hotmail.com.au>
2019-05-02 16:52:03 +1000

    Added line drawing to Line Class

--- src/Line.java
+++ src/Line.java
@@ -1,14 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class Line {
     private double x1;
     private double x2;
     private double y1;
     private double y2;
 
-    public Line(double x1, double y1, double x2, double y2) {
+    public Line(double x1, double y1, double x2, double y2, JPanel panel) {
         this.x1 = x1;
         this.x2 = x2;
         this.y1 = y1;
         this.y2 = y2;
+
+        Graphics g = panel.getGraphics();
+        g.drawLine((int) x1,(int)y1,(int)x2,(int)y2);
     }
 
     public String lineOutputFormatted() {
