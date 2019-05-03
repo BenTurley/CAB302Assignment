d3ed57d1abb06afcfafa27ad7fd6dc02c10a2ad2
ChristopherThomas <49484488+spookydinosaur@users.noreply.github.com>
2019-05-03 17:30:15 +1000

    Added comments for functions

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -17,21 +17,32 @@ public class Rectangle {
         this.panel = panel;
     }
 
+    /**
+     * Swaps x1 and x2 when called
+     */
     private void xSwap(){
         int temp = (int) this.x2;
         this.x2 = this.x1;
         this.x1 = temp;
     }
 
+    /**
+     * Swaps y1 and y2 when called
+     */
     private void ySwap(){
         int temp = (int) this.y2;
         this.y2 = this.y1;
         this.y1 = temp;
     }
 
+    /**
+     * Draws rectangle from x1,y2 after adjustment to account for the initial input always being top left of square
+     */
     public void DrawRectangle() {
         int xDiff = (int) Math.abs(x2-x1);
         int yDiff = (int) Math.abs(y2-y1);
+
+
 
         if((this.x1 > this.x2) && (this.y1< this.y2)){
             xSwap();
@@ -48,6 +59,10 @@ public class Rectangle {
         g.drawRect((int) x1,(int)y1,xDiff,yDiff);
     }
 
+    /**
+     * Returns the shape and the x-cords and y-cords for top left and bottom right in a String format
+     * @return The coordinates and specifies it's a Rectangle
+     */
     public String RectangleOutputFormatted() {
         return "RECTANGLE " + x1 + " " + y1 + " " + x2 + " " + y2;
     }
