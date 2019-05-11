b719be0b8a2fde2f500a3c1a362fc87b99a71e94
chris <chris.thomas@hotmail.com.au>
2019-05-11 10:28:58 +1000

    Removed error: public void Vector()

---
+++ src/RectangleVector.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RectangleVector implements Vector {
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+    private JPanel panel;
+
+    public RectangleVector(double x1, double y1, double x2, double y2, JPanel panel){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    /**
+     * Swaps x1 and x2 when called
+     */
+    private void xSwap(){
+        int temp = (int) this.x2;
+        this.x2 = this.x1;
+        this.x1 = temp;
+    }
+
+    /**
+     * Swaps y1 and y2 when called
+     */
+    private void ySwap(){
+        int temp = (int) this.y2;
+        this.y2 = this.y1;
+        this.y1 = temp;
+    }
+
+    @Override
+    public void DrawVector() {
+        int xDiff = (int) Math.abs(x2-x1);
+        int yDiff = (int) Math.abs(y2-y1);
+
+        if((this.x1 > this.x2) && (this.y1< this.y2)){
+            xSwap();
+        }
+        else if((this.x1 < this.x2) && (this.y1 > this.y2)){
+            ySwap();
+        }
+        else if((this.x1 > this.x2) && (this.y1 > this.y2)){
+            ySwap();
+            xSwap();
+        }
+
+        Graphics g = panel.getGraphics();
+        g.drawRect((int) x1,(int)y1,xDiff,yDiff);
+    }
+
+    @Override
+    public String VectorOutputFormatted() {
+        return "RECTANGLE " + x1 + " " + y1 + " " + x2 + " " + y2;
+    }
+}
