3ce98d44f33cbf84c60f1f66ce341690c39f2309
ChristopherThomas <49484488+spookydinosaur@users.noreply.github.com>
2019-05-17 19:18:22 +1000

    Introduced new class that scales the vectors by multiplying their 0-1 values to the respective size of the screen and returns the new value by calling x1, y1, x2, y2

---
+++ src/VectorScale.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+
+public class VectorScale {
+    private double x1;
+    private double y1;
+    private double x2;
+    private double y2;
+    private JPanel panel;
+    private double panelHeight;
+    private double panelWidth;
+
+    public VectorScale(double x1, double y1, double x2, double y2, JPanel panel){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.panel = panel;
+        this.panelHeight = panel.getSize().height;
+        this.panelWidth = panel.getSize().width;
+
+    }
+
+    public double x1(){
+        return x1 * panelWidth;
+    }
+
+    public double y1(){
+        return y1 * panelHeight;
+    }
+
+    public double x2(){
+        return x2 * panelWidth;
+    }
+
+    public double y2(){
+        return y2 * panelHeight;
+    }
+}
