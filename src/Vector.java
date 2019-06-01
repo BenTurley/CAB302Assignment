0de684eb14f4987ed4ede4b9167a08111c6d1b3c
ChristopherThomas <49484488+spookydinosaur@users.noreply.github.com>
2019-06-01 16:40:11 +1000

    Added Javadoc-styled comments to Vector interface

--- src/Vector.java
+++ src/Vector.java
@@ -1,9 +1,26 @@
 import java.awt.*;
 
+/**
+ * Interface class for Vector Shapes
+ */
 public interface Vector {
     //void Vector();
+
+    /**
+     * Changes the outline colour of the Shape for when it's drawn
+     * @param colour Colour of outline
+     */
     void SetColour(Color colour);
+
+    /**
+     * Draws the Shape
+     */
     void DrawVector(); //Alter to include Color variable input for pen colour
     //void FillVector(String colour);
+
+    /**
+     * Formats the Shapes's Vector coordinates in VEC language format
+     * @return String of formatted coordinates of Shape Vector
+     */
     String VectorOutputFormatted();
 }
