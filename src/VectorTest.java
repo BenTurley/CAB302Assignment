4b6b2b410b4e34ad7ce3948395baa3bee56843b5
ChristopherThomas <49484488+spookydinosaur@users.noreply.github.com>
2019-06-01 17:08:58 +1000

    Added test case ofr Rectangle and Ellipse to test their output format

--- src/VectorTest.java
+++ src/VectorTest.java
@@ -8,12 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class VectorTest {
     JPanel testPanel;
     Vector line;
+    Vector rectangle;
+    Vector ellipse;
 
     @BeforeEach
     public void Setup() {
         testPanel = new JPanel();
         testPanel.setSize(100, 100);
         line = null;
+        rectangle = null;
+        ellipse = null;
 
     }
 
@@ -24,6 +28,26 @@ class VectorTest {
     public void TestLineOutput() {
         line = new LineVector(0,0,50,50, testPanel);
         assertEquals("LINE 0.00 0.00 0.50 0.50",line.VectorOutputFormatted());
+
+    }
+
+    /*
+    Test 2: Rectangle Vector Output
+    */
+    @Test
+    public void TestRectangleOutput() {
+        rectangle = new RectangleVector(0,0,50,50, testPanel);
+        assertEquals("RECTANGLE 0.00 0.00 0.50 0.50",rectangle.VectorOutputFormatted());
+
+    }
+
+    /*
+    Test 3: Ellipse Vector Output
+    */
+    @Test
+    public void TestEllipseOutput() {
+        ellipse = new EllipseVector(0,0,50,50, testPanel);
+        assertEquals("ELLIPSE 0.00 0.00 0.50 0.50",ellipse.VectorOutputFormatted());
 
     }
 }
